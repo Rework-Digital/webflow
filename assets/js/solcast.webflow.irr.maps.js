ae2f845994dfb61a9b707f83de73275af859365e
Rework-Digital <liam@reworkdigital.com.au>
2024-06-07 16:03:45 +1000

    WTF Testing

--- assets/js/solcast.webflow.irr.maps.js
+++ assets/js/solcast.webflow.irr.maps.js
@@ -54,37 +54,37 @@ function fetchLatestVideoUrl() {
 
 // /* Location Images */
 // Function to update the image source with the thumbnail URL from the API response
-function updateImageSrc(locationId) {
-  // Construct the API URL using the provided location ID
-  const apiUrlPre = 'https://api.solcast.com.au/media/';
-  const apiUrlPost = '?format=json';
-  const apiUrl = apiUrlPre + locationId + apiUrlPost;
-  //const apiUrl = `https://api.solcast.com.au/media/${locationId}?format=json`;
+// function updateImageSrc(locationId) {
+//   // Construct the API URL using the provided location ID
+//   const apiUrlPre = 'https://api.solcast.com.au/media/';
+//   const apiUrlPost = '?format=json';
+//   const apiUrl = apiUrlPre + locationId + apiUrlPost;
+//   //const apiUrl = `https://api.solcast.com.au/media/${locationId}?format=json`;
 
-  // Perform the fetch request to the API
-  fetch(apiUrl)
-    .then(response => response.json()) // Parse the JSON from the response
-    .then(data => {
-      if (data.files && data.files.length > 0) {
-        // Assuming you want the first file's thumbnail URL
-        const thumbnailUrl = data.files[0].thumbnail_url;
+//   // Perform the fetch request to the API
+//   fetch(apiUrl)
+//     .then(response => response.json()) // Parse the JSON from the response
+//     .then(data => {
+//       if (data.files && data.files.length > 0) {
+//         // Assuming you want the first file's thumbnail URL
+//         const thumbnailUrl = data.files[0].thumbnail_url;
 
-        // Find the image element by the location ID and update its src attribute
-        const imageElement = document.getElementById(locationId);
-        if (imageElement) {
-          imageElement.src = thumbnailUrl;
-          console.log(`Image src updated to: ${thumbnailUrl}`); // Log the new src URL
-        } else {
-          console.error('Image element not found for the provided location ID');
-        }
-      } else {
-        console.error('No files found in the API response');
-      }
-    })
-    .catch(error => {
-      console.error('Failed to fetch data from API:', error);
-    });
-}
+//         // Find the image element by the location ID and update its src attribute
+//         const imageElement = document.getElementById(locationId);
+//         if (imageElement) {
+//           imageElement.src = thumbnailUrl;
+//           console.log(`Image src updated to: ${thumbnailUrl}`); // Log the new src URL
+//         } else {
+//           console.error('Image element not found for the provided location ID');
+//         }
+//       } else {
+//         console.error('No files found in the API response');
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Failed to fetch data from API:', error);
+//     });
+// }
 
 // /* Move Glabal Card to First Position */
 // // Function to add a class to a div with a specific custom attribute value
