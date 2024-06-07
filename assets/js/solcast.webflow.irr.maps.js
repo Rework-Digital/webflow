ba32748249cc94ef302d73e5fdf10f06800a6bce
Rework-Digital <liam@reworkdigital.com.au>
2024-06-07 16:15:46 +1000

    fix

--- assets/js/solcast.webflow.irr.maps.js
+++ assets/js/solcast.webflow.irr.maps.js
@@ -51,7 +51,6 @@ function fetchLatestVideoUrl() {
     });
 }
 
-
 // /* Location Images */
 // Function to update the image source with the thumbnail URL from the API response
 function updateImageSrc(locationId) {
@@ -59,12 +58,12 @@ function updateImageSrc(locationId) {
   const apiUrlPre = 'https://api.solcast.com.au/media/';
   const apiUrlPost = '?format=json';
   const apiUrl = apiUrlPre + locationId + apiUrlPost;
-  //const apiUrl = `https://api.solcast.com.au/media/${locationId}?format=json`;
+  // const apiUrl = `https://api.solcast.com.au/media/${locationId}?format=json`;
 
   // Perform the fetch request to the API
   fetch(apiUrl)
-    .then(response => response.json()) // Parse the JSON from the response
-    .then(data => {
+    .then((response) => response.json()) // Parse the JSON from the response
+    .then((data) => {
       if (data.files && data.files.length > 0) {
         // Assuming you want the first file's thumbnail URL
         const thumbnailUrl = data.files[0].thumbnail_url;
@@ -73,18 +72,17 @@ function updateImageSrc(locationId) {
         const imageElement = document.getElementById(locationId);
         if (imageElement) {
           imageElement.src = thumbnailUrl;
-          //console.log(`Image src updated to: ${thumbnailUrl}`); // Log the new src URL
+          // console.log(`Image src updated to: ${thumbnailUrl}`); // Log the new src URL
         } else {
-          return;
-          //console.error('Image element not found for the provided location ID');
+
+          // console.error('Image element not found for the provided location ID');
         }
       } else {
-        return;
-        //console.error('No files found in the API response');
+
+        // console.error('No files found in the API response');
       }
-    })
-    return;
-    //.catch(error => {
-      //console.error('Failed to fetch data from API:', error);
     });
+
+  // .catch(error => {
+  // console.error('Failed to fetch data from API:', error);
 }
